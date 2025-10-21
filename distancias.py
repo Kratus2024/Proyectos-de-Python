f7196a999680b7218ff94ef7d952d8013439bd82
Jhon Floki <espartahacker@gmail.com>
2025-10-20 21:42:58 -0500

    Update and rename apolo_1.py to distancias.py

--- distancias.py
+++ distancias.py
@@ -1,3 +1,7 @@
+#El objetivo de este programa es calcular la distancia entre puntos geográficos medinate coordenadas (latitud, longitud).
+# Para ello importamos la libreria geopy.
+#Solicitamos al usuario que introduzca dichas coordenadas entre los puntos para asi calcular la distancia en estecaso en kilometros.
+
 from geopy.distance import distance
 
 longitud_1 = float(input("Ingrese la longitud del primer punto: "))
@@ -11,5 +15,6 @@ latitud_2 = float(input("Ingrese la latitud del segundo punto: "))
 punto2 = (latitud_2, longitud_2)
 
 distancia = distance(punto1, punto2).kilometers
+
 
 print(f"La distancia entre las coordenadas es de {distancia:2f} km")
